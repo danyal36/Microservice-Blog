0ba30122e37d4bb533712464aa4cf9632c98eae3
danyal36 <ch.danyal36@gmail.com>
2024-05-27 18:39:06 +0500

    Feat: Docker with kuber implementation

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -3,17 +3,21 @@ import React from "react";
 const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
     let content;
+    switch (comment.status) {
+      case ("approved"):
+        content = comment.content;
+        break;
+      
+      case ("pending"):
+        content = "This comment is awaiting moderation";
+        break;
 
-    if (comment.status === "approved") {
-      content = comment.content;
-    }
+      case ("rejected"):
+        content = "This comment has been rejected";
+        break;
 
-    if (comment.status === "pending") {
-      content = "This comment is awaiting moderation";
-    }
-
-    if (comment.status === "rejected") {
-      content = "This comment has been rejected";
+      default:
+        break;
     }
 
     return <li key={comment.id}>{content}</li>;
